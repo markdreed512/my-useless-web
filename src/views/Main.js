218e70cd04e81474d4308b675c5e90523d91caf0
Mark Reed <mreed@Admins-MacBook-Pro.local>
2021-08-27 12:03:54 -0500

    react router

---
+++ src/views/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    Confetti
+                </li>
+                <li>
+                    Self-Avoiding Walk
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Main
